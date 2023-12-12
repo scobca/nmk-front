357ba54034119790d24684991057681fe17b29c7
scobca <scobca18@yandex.ru>
2023-12-12 22:41:01 +0300

    Importing animate.css, AOS animations

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+//importing local styles
 import '@/assets/styles/main.css'
 import '@/assets/styles/colors.css'
+
+//importing animations libraries
+import 'aos/dist/aos.css';
+import 'animate.css';
+
+//@ts-ignore
+import AOS from 'aos';
+AOS.init()
 
 createApp(App).use(store).use(router).mount('#app')
